611655a97210d8a44475ab620f2a5d0302e5068d
root <gogia.prateek@hotmail.com>
2018-08-04 16:05:42 +0530

    Fixed the latitude and longitude issue of map

--- src/components/ChooseFence.js
+++ src/components/ChooseFence.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, GeoJSONLayer } from "react-mapbox-gl";
 import DrawControl from "react-mapbox-gl-draw";
 
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import info from "../config/info";
+import geoJSON from "../config/geoJSON";
 
 const Map = ReactMapboxGl({
   accessToken: info.mapBoxAccessToken
-    
 });
+const center = [72.4997, 22.9960];
 
 class ChooseFence extends Component {
   onDrawCreate = ({ features }) => {
@@ -19,6 +20,7 @@ class ChooseFence extends Component {
   onDrawUpdate = ({ features }) => {
     console.log({ features });
   };
+
   constructor(props) {
     super(props);
   }
@@ -34,12 +36,22 @@ class ChooseFence extends Component {
         <Map
           style="mapbox://styles/mapbox/outdoors-v9" // eslint-disable-line
           containerStyle={{ height: "400px", width: "100vw" }}
+          center={center}
+          zoom={[13]}
         >
           <DrawControl
             position="top-left"
             onDrawCreate={this.onDrawCreate}
             onDrawUpdate={this.onDrawUpdate}
           />
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}
+          >
+            {/* <Feature coordinates={[23.0225, -72.5714]} /> */}
+            <GeoJSONLayer data={geoJSON} />
+          </Layer>
         </Map>
       </div>
     );
