48077b396fc0029bd719c35cd4b322859bf8fe79
root <gogia.prateek@hotmail.com>
2018-08-04 15:30:29 +0530

    Implemented MapBox Maps

--- src/components/ChooseFence.js
+++ src/components/ChooseFence.js
@@ -1,99 +1,46 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import Maps from "./Maps";
-import PlacesAutocomplete, {
-  geocodeByAddress,
-  getLatLng
-} from "react-places-autocomplete";
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import info from "../config/info";
+
+const Map = ReactMapboxGl({
+  accessToken: info.mapBoxAccessToken
+    
+});
 
 class ChooseFence extends Component {
+  onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  onDrawUpdate = ({ features }) => {
+    console.log({ features });
+  };
   constructor(props) {
     super(props);
-    this.state = {
-      address: "",
-      gmapsLoaded: false
-    };
   }
 
-  handleChange = address => {
-    this.setState({ address });
-  };
-
-  // initMap = () => {
-  //   this.setState({
-  //     gmapsLoaded: true
-  //   });
-  // };
-  // componentWillMount() {
-  //   window.initMap = this.initMap;
-  //   const gmapScriptEl = document.createElement(`script`);
-  //   gmapScriptEl.src = `https://maps.googleapis.com/maps/api/js?key=&libraries=places&callback=initMap`;
-  //   document
-  //     .querySelector(`body`)
-  //     .insertAdjacentElement(`beforeend`, gmapScriptEl);
-  // }
-
-  handleSelect = address => {
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log("Success", latLng))
-      .catch(error => console.error("Error", error));
-  };
   render() {
     return (
       <div>
         <Header />
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h3>Please choose fence</h3>
-            </div>
-          </div>
-          <PlacesAutocomplete
-            value={this.state.address}
-            onChange={this.handleChange}
-            onSelect={this.handleSelect}
-          >
-            {({
-              getInputProps,
-              suggestions,
-              getSuggestionItemProps,
-              loading
-            }) => (
-              <div>
-                <input
-                  {...getInputProps({
-                    placeholder: "Search Places ...",
-                    className: "location-search-input"
-                  })}
-                />
-                <div className="autocomplete-dropdown-container">
-                  {loading && <div>Loading...</div>}
-                  {suggestions.map(suggestion => {
-                    const className = suggestion.active
-                      ? "suggestion-item--active"
-                      : "suggestion-item";
-                    // inline style for demonstration purpose
-                    const style = suggestion.active
-                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                      : { backgroundColor: "#ffffff", cursor: "pointer" };
-                    return (
-                      <div
-                        {...getSuggestionItemProps(suggestion, {
-                          className,
-                          style
-                        })}
-                      >
-                        <span>{suggestion.description}</span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            )}
-          </PlacesAutocomplete>
-          {/* <Maps /> */}
+        <div className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Create a Fence</h2>
         </div>
+        <Map
+          style="mapbox://styles/mapbox/outdoors-v9" // eslint-disable-line
+          containerStyle={{ height: "400px", width: "100vw" }}
+        >
+          <DrawControl
+            position="top-left"
+            onDrawCreate={this.onDrawCreate}
+            onDrawUpdate={this.onDrawUpdate}
+          />
+        </Map>
       </div>
     );
   }
